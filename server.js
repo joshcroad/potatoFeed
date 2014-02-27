7e86488bda21eb10746fc4a352d281406b7f2748
Joshua Croad <joshcroad@gmail.com>
2014-02-27 17:36:02 +0000

    Updated port location for heroku

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var http    = require('http');
 var server  = connect()
 .use(connect.static(__dirname + '/public'));
 
-http.createServer(server).listen(3000);
+http.createServer(server).listen(process.env.PORT || 3000);
